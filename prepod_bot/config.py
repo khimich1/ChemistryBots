4baf5aad642b8b4b7611c32119a73f689ff82c92
khimich1 <smirnov.roman.18@bk.ru>
2025-09-24 16:33:44 +0300

    Обновление конфигурации prepod_bot

--- prepod_bot/config.py
+++ prepod_bot/config.py
@@ -13,6 +13,7 @@ ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "").split(","))) if os.getenv("
 
 # Если переменные окружения не заданы, используем дефолтные базы из shared/
 DB_PATH = os.getenv("DB_PATH") or os.path.join(_SHARED_DIR, "test_answers.db")
-TESTS_DB_PATH = os.getenv("TESTS_DB_PATH") or os.path.join(_SHARED_DIR, "tests1.db")
+# Всегда используем актуальную базу вопросов
+TESTS_DB_PATH = os.getenv("TESTS_DB_PATH") or os.path.join(_SHARED_DIR, "tests_updated.db")
 
 ONLINE_WINDOW_MINUTES = int(os.getenv("ONLINE_WINDOW_MINUTES", 10))
